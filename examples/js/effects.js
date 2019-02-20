7015ff941b5d1647d326520ef5b3e03ebe807274
OrrinBlake <orrinblake@gmail.com>
2019-02-20 11:48:48 +0000

    connect leap to effect

---
+++ examples/js/effects.js
@@ -0,0 +1,25 @@
+function makeDistortionCurve( amount ) {
+    var k = typeof amount === 'number' ? amount : 0,
+      n_samples = 44100,
+      curve = new Float32Array(n_samples),
+      deg = Math.PI / 180,
+      i = 0,
+      x;
+    for ( ; i < n_samples; ++i ) {
+      x = i * 2 / n_samples - 1;
+      curve[i] = ( 3 + k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) );
+    }
+    return curve;
+  };
+
+function playSineWave2(){
+  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+  // create Oscillator node
+  var oscillator = audioCtx.createOscillator();
+
+  oscillator.type = 'square';
+  oscillator.frequency.setValueAtTime(440, audioCtx.currentTime); // value in hertz
+  oscillator.connect(audioCtx.destination);
+  oscillator.start();
+}
