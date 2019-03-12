2403ee4d3499be64c0f11bd109ba4f4b80b1f71f
OrrinBlake <orrinblake@gmail.com>
2019-03-12 19:41:50 +0000

    fix waves crackling

--- machince/js/effectsHelper.js
+++ machince/js/effectsHelper.js
@@ -1,3 +1,6 @@
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  var context = new AudioContext();
+
 function makeDistortionCurve( amount ) {
     var k = typeof amount === 'number' ? amount : 0,
       n_samples = 44100,
@@ -12,12 +15,67 @@ function makeDistortionCurve( amount ) {
     return curve;
   };
 
-function base64ToArrayBuffer(base64) {
-    var binaryString = window.atob(base64);
-    var len = binaryString.length;
-    var bytes = new Uint8Array(len);
-    for (var i = 0; i < len; i++)        {
-        bytes[i] = binaryString.charCodeAt(i);
+  var bufferSize = 16384;
+  var moogEffect = (function() {
+    var node = context.createScriptProcessor(bufferSize, 1, 1);
+    var in1, in2, in3, in4, out1, out2, out3, out4;
+    in1 = in2 = in3 = in4 = out1 = out2 = out3 = out4 = 0.0;
+    node.cutoff = 0.065; // between 0.0 and 1.0
+    node.resonance = 3.99; // between 0.0 and 4.0
+    node.onaudioprocess = function(e) {
+        var input = e.inputBuffer.getChannelData(0);
+        var output = e.outputBuffer.getChannelData(0);
+        var f = node.cutoff * 1.16;
+        var fb = node.resonance * (1.0 - 0.15 * f * f);
+        for (var i = 0; i < bufferSize; i++) {
+            input[i] -= out4 * fb;
+            input[i] *= 0.35013 * (f*f)*(f*f);
+            out1 = input[i] + 0.3 * in1 + (1 - f) * out1; // Pole 1
+            in1 = input[i];
+            out2 = out1 + 0.3 * in2 + (1 - f) * out2; // Pole 2
+            in2 = out1;
+            out3 = out2 + 0.3 * in3 + (1 - f) * out3; // Pole 3
+            in3 = out2;
+            out4 = out3 + 0.3 * in4 + (1 - f) * out4; // Pole 4
+            in4 = out3;
+            output[i] = out4;
+        }
     }
-    return bytes.buffer;
-}
+    return node;
+})();
+
+var bitCrusherEffect = (function() {
+    var node = context.createScriptProcessor(bufferSize, 1, 1);
+    node.bits = 4; // between 1 and 16
+    node.normfreq = 0.1; // between 0.0 and 1.0
+    var step = Math.pow(1/2, node.bits);
+    var phaser = 0;
+    var last = 0;
+    node.onaudioprocess = function(e) {
+        var input = e.inputBuffer.getChannelData(0);
+        var output = e.outputBuffer.getChannelData(0);
+        for (var i = 0; i < bufferSize; i++) {
+            phaser += node.normfreq;
+            if (phaser >= 1.0) {
+                phaser -= 1.0;
+                last = step * Math.floor(input[i] / step + 0.5);
+            }
+            output[i] = last;
+        }
+    };
+    return node;
+})();
+
+var lowFilterPassEffect = (function() {
+    var lastOut = 0.0;
+    var node = context.createScriptProcessor(bufferSize, 1, 1);
+    node.onaudioprocess = function(e) {
+        var input = e.inputBuffer.getChannelData(0);
+        var output = e.outputBuffer.getChannelData(0);
+        for (var i = 0; i < bufferSize; i++) {
+            output[i] = (input[i] + lastOut) / 2.0;
+            lastOut = output[i];
+        }
+    }
+    return node;
+})();
